a5af342b810325ee31e3411e298a4bb4785ebb53
Nathan Solomon <nbsolomon4559@gmail.com>
2020-01-08 23:50:27 -0600

    extended directions list

--- src/mouse/utility/Constants.java
+++ src/mouse/utility/Constants.java
@@ -5,7 +5,7 @@ import battlecode.common.RobotType;
 
 public class Constants {
 
-    public static Direction[] DIRECTIONS = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
+    public static Direction[] DIRECTIONS = {Direction.NORTH, Direction.NORTHEAST, Direction.EAST, Direction.SOUTHEAST,Direction.SOUTH, Direction.SOUTHWEST, Direction.WEST, Direction.NORTHWEST};
     static RobotType[] SPAWNED_BY_MINER = {RobotType.REFINERY, RobotType.VAPORATOR, RobotType.DESIGN_SCHOOL,
             RobotType.FULFILLMENT_CENTER, RobotType.NET_GUN};
 

274ab82498c381a04e90c6a89d50208b431fe63c
Nathan Solomon <nbsol@Nathans-MacBook-Pro.local>
2020-01-10 20:34:09 -0600

    renamed to C3-PO

---
+++ src/c3po/units/genericvaporator/GenericVaporator.java
@@ -0,0 +1,15 @@
+package c3po.units.genericvaporator;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+import c3po.base.Building;
+
+public class GenericVaporator extends Building {
+    public GenericVaporator(RobotController rc){
+        super(rc);
+    }
+
+    public void turn() throws GameActionException{
+
+    }
+}
--- src/mouse/base/Building.java
+++
@@ -1,9 +0,0 @@
-package mouse.base;
-
-import battlecode.common.RobotController;
-
-public abstract class Building extends Unit {
-    public Building(RobotController rc){
-        super(rc);
-    }
-}

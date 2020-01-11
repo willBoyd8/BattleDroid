184237555350f7d67220fa19e1739d9ba738ce52
Nathan Solomon <nbsolomon4559@gmail.com>
2020-01-11 01:21:08 -0600

    patched bug where hq wouldn't do anything (#32)

--- src/c3po/units/assassindroid/AssassinDroid.java
+++ src/c3po/units/assassindroid/AssassinDroid.java
@@ -2,6 +2,7 @@ package c3po.units.assassindroid;
 
 import battlecode.common.*;
 import c3po.base.Building;
+import c3po.utility.ActionHelper;
 
 
 public class AssassinDroid extends Building {
@@ -11,19 +12,6 @@ public class AssassinDroid extends Building {
     }
 
     public void turn() throws GameActionException {
-        RobotInfo[] targets = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
-        int nearestDist = Integer.MAX_VALUE;
-        RobotInfo nearestDrone = null;
-        if (!(targets[0] == null)){
-            for (int i = 0; i < targets.length; i++){
-                if (targets[i].location.distanceSquaredTo(rc.getLocation()) < nearestDist){
-                    nearestDist = targets[i].location.distanceSquaredTo(rc.getLocation());
-                    nearestDrone = targets[i];
-                }
-            }
-            if(rc.canShootUnit(nearestDrone.ID)) {
-                rc.shootUnit(nearestDrone.ID);
-            }
-        }
+        ActionHelper.tryShoot(rc);
     }
 }

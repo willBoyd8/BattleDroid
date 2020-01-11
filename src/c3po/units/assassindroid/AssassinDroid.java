f21e434b070810d24b89567d58f472d794b3f957
Nathan Solomon <nbsolomon4559@gmail.com>
2020-01-10 23:04:49 -0600

    improved shooting. HQs now shoot as well

--- src/c3po/units/assassindroid/AssassinDroid.java
+++ src/c3po/units/assassindroid/AssassinDroid.java
@@ -12,7 +12,7 @@ public class AssassinDroid extends Building {
 
     public void turn() throws GameActionException {
         RobotInfo[] targets = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
-        int nearestDist = 9999;
+        int nearestDist = Integer.MAX_VALUE;
         RobotInfo nearestDrone = null;
         if (!(targets[0] == null)){
             for (int i = 0; i < targets.length; i++){

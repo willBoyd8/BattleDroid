44de171f0ea546aa3f882752d6f85abb18814922
Nathan Solomon <nbsolomon4559@gmail.com>
2020-01-12 13:19:29 -0600

    bot now shoots at only enemy drones

--- src/droideka/utility/ActionHelper.java
+++ src/droideka/utility/ActionHelper.java
@@ -109,7 +109,7 @@ public class ActionHelper {
         RobotInfo nearestDrone = null;
         if (!(targets[0] == null)){
             for (int i = 0; i < targets.length; i++){
-                if (targets[i].location.distanceSquaredTo(rc.getLocation()) < nearestDist){
+                if (targets[i].getType() == RobotType.DELIVERY_DRONE && targets[i].location.distanceSquaredTo(rc.getLocation()) < nearestDist){
                     nearestDist = targets[i].location.distanceSquaredTo(rc.getLocation());
                     nearestDrone = targets[i];
                 }

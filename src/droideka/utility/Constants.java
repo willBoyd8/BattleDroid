30a0ff9312607ca6c3e1907201e9083b623f5773
Nathan Solomon <nbsolomon4559@gmail.com>
2020-01-12 20:57:57 -0600

    Bugfixes (#44)
    * small error fixes
    
    * Fixed mistake in section of code for determining what quadrant friendly HQ is in
    
    Co-authored-by: Jonathan Lorray <43081271+NotChopsticks@users.noreply.github.com>

--- src/droideka/utility/Constants.java
+++ src/droideka/utility/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     static RobotType[] SPAWNED_BY_MINER = {RobotType.REFINERY, RobotType.VAPORATOR, RobotType.DESIGN_SCHOOL,
             RobotType.FULFILLMENT_CENTER, RobotType.NET_GUN};
     public static int WALL_START_ROUND = 400;
-    public static boolean DEBUGGER_ENABLE = false;
+    public static boolean DEBUGGER_ENABLE = true;
     public static int RAID_START_ROUND = 1700;
     public static int LANDSCAPERS_ON_WALL = 15;
     public static int MIN_REFINERY_SPREAD_DISTANCE = RobotType.REFINERY.pollutionRadiusSquared;

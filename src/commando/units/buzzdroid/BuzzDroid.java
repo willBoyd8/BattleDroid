8159b5a931f0d7649faa7ec95d688d34a44e6bff
Nathan Solomon <nbsol@Nathans-MacBook-Pro.local>
2020-01-15 20:44:47 -0600

    made some changes?

--- src/commando/units/buzzdroid/BuzzDroid.java
+++ src/commando/units/buzzdroid/BuzzDroid.java
@@ -3,16 +3,12 @@ package commando.units.buzzdroid;
 import battlecode.common.*;
 import commando.base.MobileUnit;
 import commando.pathing.Simple;
-import commando.utility.ActionHelper;
-import commando.utility.Constants;
-import commando.utility.DebugHelper;
-import commando.utility.DroidList;
+import commando.utility.*;
 
 
 public class BuzzDroid extends MobileUnit {
     public DroneState state;
     public DroidList<MapLocation> enemyHQLocations;
-    public int homeQuad;
     public RaidState raidState;
     public RobotInfo holding;
     public MapLocation nearestWater;
@@ -22,8 +18,7 @@ public class BuzzDroid extends MobileUnit {
         super(rc);
         state = DroneState.GENERATE;
         targetLocation = null;
-        enemyHQLocations = new DroidList<MapLocation>();
-        homeQuad = 0;
+        enemyHQLocations = new DroidList<>();
         raidState = null;
         holding = null;
         nearestWater = null;
@@ -35,8 +30,7 @@ public class BuzzDroid extends MobileUnit {
         super(unit);
         state = DroneState.GENERATE;
         targetLocation = null;
-        enemyHQLocations = new DroidList<MapLocation>();
-        homeQuad = 0;
+        enemyHQLocations = new DroidList<>();
         raidState = null;
         holding = null;
         nearestWater = null;
@@ -133,70 +127,9 @@ public class BuzzDroid extends MobileUnit {
     }
 
     private void generate() throws GameActionException {
-        int mapWidth = rc.getMapWidth();
-        int mapHeight = rc.getMapHeight();
+        enemyHQLocations.addAll(Unsorted.generatePossibleEnemyHQLocation(hqLocation, rc));
 
-        //Figure out Home Quadrant
-        if ((hqLocation.x > (mapWidth/2))&&(hqLocation.y < (mapHeight/2))){
-            homeQuad = 4;
-        }
-        else if ((hqLocation.x < (mapWidth/2))&&(hqLocation.y > (mapHeight/2))){
-            homeQuad = 2;
-        }
-        else if ((hqLocation.x < (mapWidth/2))&&(hqLocation.y < (mapHeight/2))){
-            homeQuad = 3;
-        }
-        else if ((hqLocation.x > (mapWidth/2))&&(hqLocation.y > (mapHeight/2))){
-            homeQuad = 1;
-        }
-
-        //Calculate Offsets and instantiate MapLocationObjects
-        int xOffset = mapWidth-hqLocation.x-1;
-        int yOffset = mapHeight-hqLocation.y-1;
-        MapLocation loc1;
-        MapLocation loc2;
-        MapLocation loc3;
-        MapLocation loc4;
-
-        //Set Potential Locations based on Home Quadrant
-        switch (homeQuad){
-            case 1:
-                loc2 = new MapLocation(xOffset, hqLocation.y);
-                loc3 = new MapLocation(xOffset, yOffset);
-                loc4 = new MapLocation(hqLocation.x, yOffset);
-                enemyHQLocations.add(loc4);
-                enemyHQLocations.add(loc3);
-                enemyHQLocations.add(loc2);
-                break;
-            case 2:
-                loc1 = new MapLocation(xOffset, hqLocation.y);
-                loc3 = new MapLocation(hqLocation.x, yOffset);
-                loc4 = new MapLocation(xOffset, yOffset);
-                enemyHQLocations.add(loc3);
-                enemyHQLocations.add(loc4);
-                enemyHQLocations.add(loc1);
-                break;
-            case 3:
-                loc1 = new MapLocation(xOffset, yOffset);
-                loc2 = new MapLocation(hqLocation.x, yOffset);
-                loc4 = new MapLocation(xOffset, hqLocation.y);
-
-                enemyHQLocations.add(loc4);
-                enemyHQLocations.add(loc1);
-                enemyHQLocations.add(loc2);
-                break;
-            case 4:
-                loc1 = new MapLocation(hqLocation.x, yOffset);
-                loc2 = new MapLocation(xOffset, yOffset);
-                loc3 = new MapLocation(xOffset, hqLocation.y);
-                enemyHQLocations.add(loc3);
-                enemyHQLocations.add(loc2);
-                enemyHQLocations.add(loc1);
-                break;
-        }
-
-
-        state = DroneState.LEAVEBASE;
+        state = BuzzDroid.DroneState.LEAVEBASE;
         leaveBase();
         return;
     }
--- src/commando/utility/Unsorted.java
+++ src/commando/utility/Unsorted.java
@@ -1,6 +1,7 @@
 package commando.utility;
 
 import battlecode.common.*;
+import commando.units.buzzdroid.BuzzDroid;
 
 
 public class Unsorted {
@@ -64,6 +65,76 @@ public class Unsorted {
         }
 
         return best;
+
+    }
+
+    public static DroidList<MapLocation> generatePossibleEnemyHQLocation(MapLocation hqLocation, RobotController rc){
+        DroidList<MapLocation> enemyHQLocations = new DroidList<>();
+        int homeQuad = 0;
+
+        int mapWidth = rc.getMapWidth();
+        int mapHeight = rc.getMapHeight();
+
+        //Figure out Home Quadrant
+        if ((hqLocation.x > (mapWidth/2))&&(hqLocation.y < (mapHeight/2))){
+            homeQuad = 4;
+        }
+        else if ((hqLocation.x < (mapWidth/2))&&(hqLocation.y > (mapHeight/2))){
+            homeQuad = 2;
+        }
+        else if ((hqLocation.x < (mapWidth/2))&&(hqLocation.y < (mapHeight/2))){
+            homeQuad = 3;
+        }
+        else if ((hqLocation.x > (mapWidth/2))&&(hqLocation.y > (mapHeight/2))){
+            homeQuad = 1;
+        }
+
+        //Calculate Offsets and instantiate MapLocationObjects
+        int xOffset = mapWidth-hqLocation.x-1;
+        int yOffset = mapHeight-hqLocation.y-1;
+        MapLocation loc1;
+        MapLocation loc2;
+        MapLocation loc3;
+        MapLocation loc4;
+
+        //Set Potential Locations based on Home Quadrant
+        switch (homeQuad){
+            case 1:
+                loc2 = new MapLocation(xOffset, hqLocation.y);
+                loc3 = new MapLocation(xOffset, yOffset);
+                loc4 = new MapLocation(hqLocation.x, yOffset);
+                enemyHQLocations.add(loc4);
+                enemyHQLocations.add(loc3);
+                enemyHQLocations.add(loc2);
+                break;
+            case 2:
+                loc1 = new MapLocation(xOffset, hqLocation.y);
+                loc3 = new MapLocation(hqLocation.x, yOffset);
+                loc4 = new MapLocation(xOffset, yOffset);
+                enemyHQLocations.add(loc3);
+                enemyHQLocations.add(loc4);
+                enemyHQLocations.add(loc1);
+                break;
+            case 3:
+                loc1 = new MapLocation(xOffset, yOffset);
+                loc2 = new MapLocation(hqLocation.x, yOffset);
+                loc4 = new MapLocation(xOffset, hqLocation.y);
+
+                enemyHQLocations.add(loc4);
+                enemyHQLocations.add(loc1);
+                enemyHQLocations.add(loc2);
+                break;
+            case 4:
+                loc1 = new MapLocation(hqLocation.x, yOffset);
+                loc2 = new MapLocation(xOffset, yOffset);
+                loc3 = new MapLocation(xOffset, hqLocation.y);
+                enemyHQLocations.add(loc3);
+                enemyHQLocations.add(loc2);
+                enemyHQLocations.add(loc1);
+                break;
+        }
+
+        return enemyHQLocations;
 
     }
 
